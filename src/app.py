fbcdbadd90252ab4e7645a60e715f72158dea09b
Imran Shitta-Bey <pickleboyonline@gmail.com>
2024-08-03 00:47:07 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+
+# TODO: Give a header, way to upload resume, then go to a separate page with the chat.
+# TODO: once chat is finished (user will click a button), score it and go to another page
+# TODO: with a score
+
 st.write("Hello world!")
 
 st.header("Erm, what the sigma?")
